6cbb9035066c64ad967da3d762fc597fe740f23b
Michael Auchter <michael.auchter@gmail.com>
2011-11-21 02:12:18 -0600

    Add default location...

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,5 +1,7 @@
 
 function weather {
+   loc=$1
+   [[ -z $loc ]] && loc=78756
    # incredibly fragile, but why not?
-   hget 'http://thefuckingweather.com/?zipcode='$1 | htmlfmt | sed -e '2d' -e '/^$/q'
+   hget 'http://thefuckingweather.com/?zipcode='$loc | htmlfmt | sed -e '2d' -e '/^$/q'
 }
