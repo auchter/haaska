e93cfd322ab4ce46c9f55ed3e9194a1dda1fc1db
Michael Auchter <michael.auchter@gmail.com>
2013-01-06 19:20:05 +0000

    Add lstap aliases

---
+++ zsh/lstap.zsh
@@ -0,0 +1,2 @@
+alias hopfields="lstap --verbose --date http://www.taplister.com/bars/4e1a2449aeb7eb0120dbf401 | tail -10 | tac"
+alias draughthouse="lstap --verbose --date http://www.taplister.com/bars/40b13b00f964a5209af31ee3 | tail -10 | tac"
