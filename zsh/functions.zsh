b3d36d1e60d49d537d81cfbfbfd7c9431948b61e
Michael Auchter <michael.auchter@gmail.com>
2013-01-16 04:49:59 +0000

    Add config files for ncmpc

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -3,5 +3,5 @@ function weather {
    loc=$1
    [[ -z $loc ]] && loc=78756
    # incredibly fragile, but why not?
-   hget 'http://thefuckingweather.com/?zipcode='$loc | htmlfmt | sed -e '2d' -e '/^$/q'
+   hget 'http://thefuckingweather.com/?where='$loc | htmlfmt | sed -e '2d' -e '/^$/q'
 }
